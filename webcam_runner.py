5f8864543715925a99547b00b5f73678f8d3b2e8
andrewsilva9 <asilva9@gatech.edu>
2017-01-18 10:30:49 -0500

    Removed face haar cascades

--- webcam_runner.py
+++ webcam_runner.py
@@ -1,27 +1,7 @@
 import cv2
 import face_detector
 
-# class CameraRunner:
-# 	def __init__(self):
-# 		self.image_width = 350
-# 		self.image_height = 350
-# 		self.capture = cv2.VideoCapture(0)
-
-
-face_finder = cv2.CascadeClassifier("cascades/haarcascade_frontalface_default.xml")
-face_finder2 = cv2.CascadeClassifier("cascades/haarcascade_alt2.xml")
-face_finder3 = cv2.CascadeClassifier("cascades/haarcascade_frontalface_alt.xml")
-face_finder4 = cv2.CascadeClassifier("cascades/haarcascade_frontalface_alt_tree.xml")
 eye_finder = cv2.CascadeClassifier("cascades/haarcascade_eye.xml")
-
-def draw_boxes(im, boxes):
-	x1 = boxes[:, 0]
-	y1 = boxes[:, 1]
-	x2 = boxes[:, 2]
-	y2 = boxes[:, 3]
-	for i in range(x1.shape[0]):
-		cv2.rectangle(im, (int(x1[i]), int(y1[i])), (int(x2[i]), int(y2[i])), (0, 255, 0), 1)
-	return im
 
 
 def run():
@@ -34,24 +14,6 @@ def run():
 		# find face bounding boxes
 		faces, _ = finder.find_faces(input_image)
 
-		# face1 = face_finder.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
-		# face2 = face_finder2.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
-		# face3 = face_finder3.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
-		# face4 = face_finder4.detectMultiScale(frame, scaleFactor=1.1, minNeighbors=10, minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
-
-		# Go over detected faces, stop at first detected face, return empty if no face.
-		# if len(face1) == 1:
-		# 	face = face1[0]
-		# if len(face2) == 1:
-		# 	face = face2[0]
-		# if len(face3) == 1:
-		# 	face = face3[0]
-		# if len(face4) == 1:
-		# 	face = face4[0]
-		# else:
-		# 	face = []
-
-		# new_img = draw_boxes(frame, faces)
 		for face in faces:
 			x1 = int(face[0])
 			y1 = int(face[1])
@@ -61,21 +23,15 @@ def run():
 			face_box = frame[y1:y2, x1:x2]
 			eyes = eye_finder.detectMultiScale(face_box)
 			for (ex, ey, ew, eh) in eyes:
-				cv2.rectangle(frame, (x1 + ex, y1 + ey), (x1 + ex + ew, y1 + ey + eh), (255, 0, 0), 2)
-		# if len(face) > 0:
-		# 	cv2.rectangle(new_img, (int(face[0]), int(face[1])), (int(face[0] + face[2]), int(face[1] + face[3])), (255, 0, 0), 1)
+				ex += x1
+				ey += y1
+				cv2.rectangle(frame, (ex, ey), (ex + ew, ey + eh), (255, 0, 0), 1)
 		cv2.imshow('frame', frame)
 
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 	capture.release()
 	cv2.destroyAllWindows()
-	# # Cut and save face
-	# # scale image
-	# gray = gray[face[1]:face[1] + face[3],
-	# 			face[0]:face[0] + face[2]]
-	# out = cv2.resize(gray, (self.image_height, self.image_width))  # Resize face so all images have same size
-	# return self.emotions[self.classifier.classify_emotion(out)]
 
 run()
 
